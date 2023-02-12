2564af37eead157c1e56d0ad6c91feeb23682549
sandywadhwa <sandy.wadhwa@gmail.com>
2023-02-12 20:57:12 +0530

    Weather App

--- frontend/public/js/product.js
+++ frontend/public/js/product.js
@@ -1,1 +1,16 @@
 console.log("Product Page JS Loaded")
+
+fetch('/api/products')
+	.then(function(res){
+		console.log(res.json());
+		console.log(res.data);
+	})
+	.then(function(d){
+		// console.log(d);
+	})
+	.catch(function(err){
+		alert(err);
+	})
+	.finally(function(){
+		console.log("Finaly");
+	});
--- server.js
+++ server.js
@@ -9,6 +9,24 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname+"/frontend/public/views/index.html");
 });
 
+app.get('/api/products', (req, res) => {
+	let products = [
+		{
+			"id" : 1,
+			"name" : "Laptop"
+		},
+		{
+			"id" : 2,
+			"name" : "Mobile"
+		},
+		{
+			"id" : 3,
+			"name" : "Tablet"
+		}
+	];
+	res.json(products);
+});
+
 app.get('/:page', (req, res) => res.sendFile(util.format("%s/frontend/public/views/%s.html", __dirname, req.params.page)));
 
 
